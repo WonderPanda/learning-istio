e72fdd8d33f1e2b9d0c2996def01e751b6dc8fef
Jesse Carter <jesse.r.carter@gmail.com>
2018-12-20 08:38:48 -0500

    Working deployment to Minikube

--- index.ts
+++ index.ts
@@ -7,3 +7,27 @@ const istioSystemNamespace = new k8s.core.v1.Namespace(istioSystem, {
     name: istioSystem
   }
 });
+
+const istio = 'istio';
+
+const istioChart = new k8s.helm.v2.Chart(
+  istio,
+  {
+    namespace: istioSystemNamespace.metadata.apply(x => x.name),
+    chart: 'istio',
+    fetchOpts: { repo: 'https://istio.io/charts/' },
+    version: '1.0.1',
+    values: {
+      gateways: {
+        ['istio-ingressgateway']: {
+          type: 'NodePort'
+        },
+        ['istio-egressgateway']: {
+          type: 'NodePort'
+        }
+      },
+      kiali: { enabled: true }
+    }
+  },
+  { dependsOn: istioSystemNamespace }
+);
